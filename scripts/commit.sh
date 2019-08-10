a543d786ba301b573fb9b8e582032ca1b28a128f
Risa Harada <supikateriburu@gmail.com>
2019-08-10 13:34:19 +0900

    :new: Add commit.sh

---
+++ scripts/commit.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -eu
+
+git_name=$(git log -1 --pretty=format:"%cn")
+git_email=$(git log -1 --pretty=format:"%ce")
+branch_name=$(git symbolic-ref --short HEAD)
+repository_name="https://${PERSONAL_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
+
+git config user.name ${git_name}
+git config user.email ${git_email}
+
+set +e
+
+git diff --exit-code --quiet -- utapri.json
+if [ 0 -ne $? ]; then
+  git add utapri.json
+  git commit -m ":shower: Sort utapri.json"
+  git push ${repository_name} ${branch_name}
+fi
+
+git diff --exit-code --quiet -- utapri.yml
+if [ 0 -ne $? ]; then
+  git add utapri.yml
+  git commit -m ":sparkles: Commit pdf"
+  git push ${repository_name} ${branch_name}
+fi
