6e8f1f8f466f67cda691f38e3761a10f10bbc473
Risa <supikateriburu@gmail.com>
2019-08-14 23:54:30 +0900

    :wrench: Fix commit condition

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -5,13 +5,16 @@ set -eu
 git_name=$(git log -1 --pretty=format:"%cn")
 git_email=$(git log -1 --pretty=format:"%ce")
 branch_name=$(git symbolic-ref --short HEAD)
-repository_name="https://${PERSONAL_TOKEN}@github.com/${GITHUB_REPOSITORY}"
+# repository_name="https://${PERSONAL_TOKEN}@github.com/${GITHUB_REPOSITORY}"
 
 git config user.name ${git_name}
 git config user.email ${git_email}
 
 set +e
 
-git add mybook/mybook.pdf
-git commit -m ":fax: Build pdf"
-git push ${repository_name} ${branch_name}
+git diff head --exit-code --quiet
+if [ 0 -ne $? ]; then
+  git add .
+  git commit -m ":fax: Build pdf"
+  git push ${repository_name} ${branch_name}
+fi
