d82a38ba41fb8855e351d92fb66d109bdd3fb335
Risa <supikateriburu@gmail.com>
2019-09-08 01:58:07 +0900

    :wrench:

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-last_commit_message=$(git log -1 --pretty=%B)
-built_commit_message=":fax: Build pdf"
-
 set +e
 
-if [ ${last_commit_message: : 15} != built_commit_message ]; then
-  cd mybook
-  rake pdf:nombre
-fi
+cd mybook
+rake pdf:nombre
